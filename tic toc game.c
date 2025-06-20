e9dc3a2f8d9fe78f9a81c457949f6bade9fdf915
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2025-06-20 09:35:46 +0530

    Update tic toc game.c

--- tic toc game.c
+++ tic toc game.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#include <string.h> // memset
 
 // Function to initialize the board
+// board is in stack as | 00 | 01 | 02 | 10 | 11 | 12 | 20 | 21 | 22 | in form of array of 9 bytes/ chars
+// So we can them all ' ' by a single for loop or memset
 void initializeBoard(char board[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board[i][j] = ' ';
+        for (int i = 0; i < 9; i++) {
+                board[i / 3][i % 3] = ' ';
+                // or
+                // *((char *)board + i) = ' ';
         }
-    }
+        // or
+        // memset(board, ' ', 9);
 }
 
 // Function to print the board
 void printBoard(char board[3][3]) {
-    printf("  0 1 2\n");
+    printf("  1 2 3\n");
     for (int i = 0; i < 3; i++) {
-        printf("%d ", i);
+        printf("%d ", i + 1);
         for (int j = 0; j < 3; j++) {
             printf("%c ", board[i][j]);
         }
@@ -21,23 +26,29 @@ void printBoard(char board[3][3]) {
     }
 }
 
+// All combination by which player can win
+char winCombos[][9] = {
+  {1, 1, 1, 0, 0, 0, 0, 0, 0}, // first row
+  {1, 0, 0, 1, 0, 0, 1, 0, 0}, // 1st column
+  {1, 0, 0, 0, 1, 0, 0, 0, 1}, // (1, 1) box to (3, 3) box, diagonal
+  {0, 0, 0, 1, 1, 1, 0, 0, 0},  // 2nd row
+  {0, 1, 0, 0, 1, 0, 0, 1, 0}, // 2nd column
+  {0, 0, 1, 0, 1, 0, 1, 0, 0}, // (1, 3) box to (3, 1) box, diagonal
+  {0, 0, 0, 0, 0, 0, 1, 1, 1}, // 3rd row
+  {0, 0, 1, 0, 0, 1, 0, 0, 1} // 3rd column
+};
 // Function to check if the current player has won
-int checkWin(char board[3][3], char player) {
-    // Check rows and columns
-    for (int i = 0; i < 3; i++) {
-        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
-            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
-            return 1; // Win
-        }
+int checkWin(char board[3][3], char player) {    // Check rows and columns
+  for (int probs = 0; probs < 8; probs++) {
+    int matched = 0;
+    for (int i = 0; i < 9; i++) {
+      if (winCombos[probs][i] * player == board[i/3][i%3])
+        matched++;
     }
-
-    // Check diagonals
-    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
-        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
-        return 1; // Win
-    }
-
-    return 0; // No win
+    if (matched == 3)
+      return 1; // player won
+  }
+  return 0; // player didn't win
 }
 
 // Function to check if the board is full
@@ -67,6 +78,8 @@ int main() {
         // Get the player's move
         printf("Player %c, enter your move (row and column): ", currentPlayer);
         scanf("%d %d", &row, &col);
+        row--;
+        col--;
 
         // Check if the move is valid
         if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
